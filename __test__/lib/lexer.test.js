5b2464fa9e8941f023b2ee073b4b65eab6e89ce8
Abhishek Kumar <akbittu@gmail.com>
2023-05-15 22:54:43 +0530

    Added unit-test cases for Lexer & Tabulator

---
+++ __test__/lib/lexer.test.js
@@ -0,0 +1,95 @@
+import Lexer from '../../lib/lexer';
+
+describe('Select Lexer', () => {
+  test('isAlphaNumeric returns true for alphanumeric characters', () => {
+    expect(Lexer.isAlphaNumeric('a')).toBe(true);
+    expect(Lexer.isAlphaNumeric('Z')).toBe(true);
+    expect(Lexer.isAlphaNumeric('5')).toBe(true);
+  });
+
+  test('isAlphaNumeric returns false for non-alphanumeric characters', () => {
+    expect(Lexer.isAlphaNumeric('.')).toBe(false);
+    expect(Lexer.isAlphaNumeric(' ')).toBe(false);
+    expect(Lexer.isAlphaNumeric('\n')).toBe(false);
+  });
+
+  test('isWhiteSpace returns true for white space characters', () => {
+    expect(Lexer.isWhiteSpace(' ')).toBe(true);
+    expect(Lexer.isWhiteSpace('\t')).toBe(true);
+    expect(Lexer.isWhiteSpace('\n')).toBe(true);
+  });
+
+  test('isWhiteSpace returns false for non-white space characters', () => {
+    expect(Lexer.isWhiteSpace('a')).toBe(false);
+    expect(Lexer.isWhiteSpace('1')).toBe(false);
+    expect(Lexer.isWhiteSpace('*')).toBe(false);
+  });
+
+  test('isParenthesis returns true for parenthesis characters', () => {
+    expect(Lexer.isParenthesis('(')).toBe(true);
+    expect(Lexer.isParenthesis(')')).toBe(true);
+  });
+
+  test('isParenthesis returns false for non-parenthesis characters', () => {
+    expect(Lexer.isParenthesis('[')).toBe(false);
+    expect(Lexer.isParenthesis(']')).toBe(false);
+    expect(Lexer.isParenthesis('{')).toBe(false);
+    expect(Lexer.isParenthesis('}')).toBe(false);
+  });
+
+  test('isBracket returns true for bracket characters', () => {
+    expect(Lexer.isBracket('[')).toBe(true);
+    expect(Lexer.isBracket(']')).toBe(true);
+  });
+
+  test('isBracket returns false for non-bracket characters', () => {
+    expect(Lexer.isBracket('(')).toBe(false);
+    expect(Lexer.isBracket(')')).toBe(false);
+    expect(Lexer.isBracket('{')).toBe(false);
+    expect(Lexer.isBracket('}')).toBe(false);
+  });
+
+  test('isComma returns true for comma characters', () => {
+    expect(Lexer.isComma(',')).toBe(true);
+  });
+
+  test('isComma returns false for non-comma characters', () => {
+    expect(Lexer.isComma(';')).toBe(false);
+    expect(Lexer.isComma('.')).toBe(false);
+    expect(Lexer.isComma('a')).toBe(false);
+  });
+
+  test('isAsterisk returns true for asterisk characters', () => {
+    expect(Lexer.isAsterisk('*')).toBe(true);
+  });
+
+  test('isAsterisk returns false for non-asterisk characters', () => {
+    expect(Lexer.isAsterisk('+')).toBe(false);
+    expect(Lexer.isAsterisk('-')).toBe(false);
+    expect(Lexer.isAsterisk('a')).toBe(false);
+  });
+
+  test('isQuote returns true for quote characters', () => {
+    expect(Lexer.isQuote('\'')).toBe(true);
+    expect(Lexer.isQuote('\"')).toBe(true);
+    expect(Lexer.isQuote('`')).toBe(true);
+  });
+
+  test('isQuote returns false for non-quote characters', () => {
+    expect(Lexer.isQuote('.')).toBe(false);
+    expect(Lexer.isQuote(',')).toBe(false);
+    expect(Lexer.isQuote('a')).toBe(false);
+  });
+
+  test('isSpecialChar returns true for specialchar characters', () => {
+    expect(Lexer.isSpecialChar('_')).toBe(true);
+    expect(Lexer.isSpecialChar('-')).toBe(true);
+    expect(Lexer.isSpecialChar('%')).toBe(true);
+  });
+
+  test('isSpecialChar returns false for non-specialchar characters', () => {
+    expect(Lexer.isSpecialChar('a')).toBe(false);
+    expect(Lexer.isSpecialChar('1')).toBe(false);
+    expect(Lexer.isSpecialChar('*')).toBe(false);
+  });
+});
---
+++ __test__/lib/tabulator.test.js
@@ -0,0 +1,204 @@
+import Tabulator from '../../lib/Tabulator';
+import YAML from 'yaml';
+
+// A sample input type #1, which takes an object with columns and rows properties where the columns property is an object that maps column ids to column titles and the rows property is an array of objects that represent the data for each row, with keys matching the column ids.
+const sampleColumnsAndRows = {
+  columns: { id: 'ID', name: 'Name', age: 'Age' },
+  rows: [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie', age: 35 }
+  ]
+};
+
+// A sample input type #2, which returns a 2D array of strings representing the tabulated data where the first element of the array is an array of column titles and the rest of the elements are arrays of row values, in the same order as the columns.
+const sampleTabulatedData = [
+  ['ID', 'Name', 'Age'],
+  [1, 'Alice', 25],
+  [2, 'Bob', 30],
+  [3, 'Charlie', 35]
+];
+
+describe('Tabulator', () => {
+  // Declare a variable to store the original console.log function
+  let originalLog;
+
+  // Use beforeEach to mock console.log before each test
+  beforeEach(() => {
+    // Save the original console.log function
+    originalLog = console.log;
+    // Replace it with a jest mock function
+    console.log = jest.fn();
+  });
+
+  // Use afterEach to restore console.log after each test
+  afterEach(() => {
+    // Restore the original console.log function
+    console.log = originalLog;
+  });
+  
+  test('toTable converts columns and rows into a 2D array', () => {
+    // Arrange
+    const { columns, rows } = sampleColumnsAndRows;
+
+    // Act
+    let table = Tabulator.toTable(columns, rows);
+
+    // Assert
+    expect(table).toEqual([
+      ['ID', 'Name', 'Age'],
+      [1, 'Alice', 25],
+      [2, 'Bob', 30],
+      [3, 'Charlie', 35]
+    ]);
+  });
+
+  test('printAsJSON prints rows as an array of objects in JSON format', () => {
+    // Arrange
+    const { columns, rows } = sampleColumnsAndRows;
+
+    // Act
+    Tabulator.printAsJSON(columns, rows);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(1); // One call for the whole array
+    expect(console.log).toHaveBeenCalledWith(
+      JSON.stringify([
+        { ID: 1, Name: 'Alice', Age: 25 },
+        { ID: 2, Name: 'Bob', Age: 30 },
+        { ID: 3, Name: 'Charlie', Age: 35 }
+      ], null, 2)
+    ); // The expected JSON output
+  });
+
+  test('printAsYAML prints rows as an array of objects in YAML format', () => {
+    // Arrange
+    const { columns, rows } = sampleColumnsAndRows;
+
+    // Act
+    Tabulator.printAsYAML(columns, rows);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(1); // One call for the whole array
+    expect(console.log).toHaveBeenCalledWith(
+      YAML.stringify([
+        { ID: 1, Name: 'Alice', Age: 25 },
+        { ID: 2, Name: 'Bob', Age: 30 },
+        { ID: 3, Name: 'Charlie', Age: 35 }
+      ])
+    ); // The expected YAML output
+  });
+
+  test('printAsCSV prints tabulated data as comma-separated values', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsCSV(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(4); // One call per row
+    expect(console.log).toHaveBeenCalledWith('ID,Name,Age'); // First row
+    expect(console.log).toHaveBeenCalledWith('1,Alice,25'); // Second row
+    expect(console.log).toHaveBeenCalledWith('2,Bob,30'); // Third row
+    expect(console.log).toHaveBeenCalledWith('3,Charlie,35'); // Fourth row
+  });
+
+  test('printAsTSV prints tabulated data as tab-separated values', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsTSV(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(4); // One call per row
+    expect(console.log).toHaveBeenCalledWith('ID\tName\tAge'); // First row
+    expect(console.log).toHaveBeenCalledWith('1\tAlice\t25'); // Second row
+    expect(console.log).toHaveBeenCalledWith('2\tBob\t30'); // Third row
+    expect(console.log).toHaveBeenCalledWith('3\tCharlie\t35'); // Fourth row
+  });
+
+  test('printAsPSV prints tabulated data as pipe-separated values', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsPSV(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(4); // One call per row
+    expect(console.log).toHaveBeenCalledWith('ID|Name|Age'); // First row
+    expect(console.log).toHaveBeenCalledWith('1|Alice|25'); // Second row
+    expect(console.log).toHaveBeenCalledWith('2|Bob|30'); // Third row
+    expect(console.log).toHaveBeenCalledWith('3|Charlie|35'); // Fourth row
+  });
+
+  test('printAsHTM prints tabulated data as an HTM table', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsHTM(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(1); // One call for the whole table
+    expect(console.log).toHaveBeenCalledWith(
+      `<table><thead><tr><th>ID</th><th>Name</th><th>Age</th></tr></thead><tbody><tr><td>1</td><td>Alice</td><td>25</td></tr><tr><td>2</td><td>Bob</td><td>30</td></tr><tr><td>3</td><td>Charlie</td><td>35</td></tr></tbody></table>`
+    ); // The expected HTML output
+  });
+
+  test('printAsHTML prints tabulated data as an HTML table', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsHTML(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(1); // One call for the whole table
+    expect(console.log).toHaveBeenCalledWith(
+      `<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Age</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Alice</td>
+      <td>25</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Bob</td>
+      <td>30</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Charlie</td>
+      <td>35</td>
+    </tr>
+  </tbody>
+</table>`
+    ); // The expected HTML output
+  });
+
+  test('printAsTable prints tabulated data as a formatted table', () => {
+    // Arrange
+    let tabulatedData = sampleTabulatedData;
+
+    // Act
+    Tabulator.printAsTable(tabulatedData);
+
+    // Assert
+    expect(console.log).toHaveBeenCalledTimes(4); // One call per row
+    expect(console.log).toHaveBeenCalledWith('ID  Name     Age  '); // First row
+    expect(console.log).toHaveBeenCalledWith('1   Alice    25   '); // Second row
+    expect(console.log).toHaveBeenCalledWith('2   Bob      30   '); // Third row
+    expect(console.log).toHaveBeenCalledWith('3   Charlie  35   '); // Fourth row
+  });
+});
