65c92fa71b8795c9313d4be7f525ac9dc599293f
Abhishek Kumar <akbittu@gmail.com>
2023-05-16 09:58:33 +0530

    Added unit-tests for WhereLexer

---
+++ __test__/lib/where/where-lexer.test.js
@@ -0,0 +1,43 @@
+import {describe, expect, test, beforeEach} from '@jest/globals';
+import WhereLexer from '../../../lib/where/where-lexer';
+
+// Define a test suite for the WhereLexer class
+describe('WhereLexer', () => {
+  // Declare a variable to hold the instance of WhereLexer
+  let whereLexer;
+
+  // Use beforeEach to create a new instance of WhereLexer before each test
+  beforeEach(() => {
+    whereLexer = new WhereLexer();
+  });
+
+  // Define some test cases with input and expected output
+  const testCases = [
+    {
+      input: 'name = "John"',
+      output: ['name', '=', '"John"']
+    },
+    {
+      input: 'age >= 18 and gender != "F"',
+      output: ['age', '>=', '18', 'and', 'gender', '!=', '"F"']
+    },
+    {
+      input: '(city = "New York" or city = "London") and country in ("USA", "UK")',
+      output: ['(', 'city', '=', '"New York"', 'or', 'city', '=', '"London"', ')', 'and', 'country', 'in', '(', '"USA"', ',', '"UK"', ')']
+    },
+    {
+      input: 'invalid expression',
+      output: ['invalid', 'expression']
+    }
+  ];
+
+  // Write a test for each test case using the test() function
+  testCases.forEach(testCase => {
+    test(`tokenize "${testCase.input}"`, () => {
+      // Call the tokenize method with the input
+      const tokens = whereLexer.tokenize(testCase.input);
+      // Expect the tokens to match the output
+      expect(tokens).toEqual(testCase.output);
+    });
+  });
+});
