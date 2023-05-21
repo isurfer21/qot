ad980c5ffc21f54e3d527ed38acdce97c97e6d08
Abhishek Kumar <akbittu@gmail.com>
2023-05-22 00:51:34 +0530

    Took the sample-table out of unit tests

--- __test__/lib/select/select-clause.test.js
+++ __test__/lib/select/select-clause.test.js
@@ -2,12 +2,14 @@ import { describe, expect, test } from '@jest/globals';
 import SelectClause from '../../../lib/select/select-clause';
 
 describe('SelectClause', () => {
+  const sampleTable = [
+    { name: 'Alice', age: 20, salary: 1000 },
+    { name: 'Bob', age: 21, salary: 2000 },
+    { name: 'Charlie', age: 23, salary: 3000 }
+  ];
+
   test('process should parse the select statement and extract the columns, aggregates and filters', () => {
-    let selectClause = new SelectClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let selectClause = new SelectClause(false, sampleTable);
     selectClause.process('name AS "Employee Name", AVG(age) AS "Average Age", DISTINCT salary');
     expect(selectClause.columns).toEqual({ age: 'Average Age', name: 'Employee Name', salary: 'salary' });
     expect(selectClause.aggregates).toEqual({ age: 'AVG' });
@@ -15,11 +17,7 @@ describe('SelectClause', () => {
   });
 
   test('process should handle wildcard and distinct keywords in the select statement', () => {
-    let selectClause = new SelectClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let selectClause = new SelectClause(false, sampleTable);
     selectClause.process('*');
     expect(selectClause.columns).toEqual({ age: 'age', name: 'name', salary: 'salary' });
     expect(selectClause.aggregates).toEqual({});
@@ -32,11 +30,7 @@ describe('SelectClause', () => {
   });
 
   test('process should exit with an error if the select statement contains invalid columns', () => {
-    let selectClause = new SelectClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let selectClause = new SelectClause(false, sampleTable);
 
     // Mock the console.error and process.exit methods
     console.error = jest.fn();
--- __test__/lib/where/where-clause.test.js
+++ __test__/lib/where/where-clause.test.js
@@ -2,12 +2,14 @@ import { describe, expect, test } from '@jest/globals';
 import WhereClause from '../../../lib/where/where-clause';
 
 describe('WhereClause', () => {
+  const sampleTable =  [
+    { name: 'Alice', age: 20, salary: 1000 },
+    { name: 'Bob', age: 21, salary: 2000 },
+    { name: 'Charlie', age: 23, salary: 3000 }
+  ];
+
   test('process should filter the rows based on the where condition', () => {
-    let whereClause = new WhereClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let whereClause = new WhereClause(false, sampleTable);
     whereClause.process('age > 20');
     expect(whereClause.filteredRows).toEqual([
       { name: 'Bob', age: 21, salary: 2000 },
@@ -16,11 +18,7 @@ describe('WhereClause', () => {
   });
 
   test('process should handle complex where conditions with logical operators', () => {
-    let whereClause = new WhereClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let whereClause = new WhereClause(false, sampleTable);
     whereClause.process('(age > 20 AND salary < 3000) OR name = "Alice"');
     expect(whereClause.filteredRows).toEqual([
       { name: 'Alice', age: 20, salary: 1000 },
@@ -29,21 +27,13 @@ describe('WhereClause', () => {
   });
 
   test('process should handle where conditions with string literals and comparison operators', () => {
-    let whereClause = new WhereClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+    let whereClause = new WhereClause(false, sampleTable);
     whereClause.process('name LIKE "A%"');
     expect(whereClause.filteredRows).toEqual([{ name: 'Alice', age: 20, salary: 1000 }]);
   });
 
-  test('process should return all rows if the where condition is empty or invalid', () => {
-    let whereClause = new WhereClause(false, [
-      { name: 'Alice', age: 20, salary: 1000 },
-      { name: 'Bob', age: 21, salary: 2000 },
-      { name: 'Charlie', age: 23, salary: 3000 }
-    ]);
+  test('process should return all rows if the where condition is invalid', () => {
+    let whereClause = new WhereClause(false, sampleTable);
     whereClause.process('foo bar');
     expect(whereClause.filteredRows).toEqual([
       { name: 'Alice', age: 20, salary: 1000 },
